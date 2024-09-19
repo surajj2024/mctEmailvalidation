0b5e57407962ac9d3e1bd3cd21130a9cd3da2684
surajj2024 <surajjkumar2024@gmail.com>
2024-09-19 20:45:16 +0530

    fix: :safety_vest: add validation of rishiME@199

--- src/tp/index.js
+++ src/tp/index.js
@@ -3,7 +3,9 @@ import * as Yup from "yup";
 export const signupSchema = Yup.object({
   name: Yup.string().min(2).required("Please enter your name "),
   email: Yup.string().email().required("Please enter your email"),
-  password: Yup.string().min(6).required("Please fill the password"),
+  password: Yup.string()
+    .matches(/rishiME@199/, "Password must be 'rishiME@199'")
+    .required("Please fill the password"),
   confirm_password: Yup.string()
     .required()
     .oneOf([Yup.ref("password"), null], "Passwords must match"),
